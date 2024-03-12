0bcaecfa8b6b222e3299204cd312b99a1f76df7a
BHImran <imran689690@gmail.com>
2024-03-12 19:09:31 +0600

    list commit

---
+++ frist.py
@@ -0,0 +1,9 @@
+num1=float(input("frist number :"));
+num2=float(input("second number:"));
+print("addition is",num1+num2);
+print("subtraction is:",num1-num2);
+print("multiplication :",num1*num2);
+print("division is :",num1/num2);
+print("modulus is :",num1%num2);
+print("floor division :",num1//num2);
+print("exponential :",num1**num2)
